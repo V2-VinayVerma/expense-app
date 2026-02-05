35156ee81557cf9fd5ccbbfe90d911d65b64341d
Vinay Verma <vinayvrma.21@gmail.com>
2026-02-05 16:53:20 +0530

    updated

---
+++ src/utility/permissions.js
@@ -0,0 +1,27 @@
+const { ADMIN_ROLE, VIEWER_ROLE, MANAGER_ROLE } = require("./userRoles");
+
+const permissions = {
+    [ADMIN_ROLE]: [
+        'user:create',
+        'user:update',
+        'user:delete',
+        'user:view',
+        'group:create',
+        'group:update',
+        'group:delete',
+        'group:view',
+    ],
+
+    [VIEWER_ROLE]: [
+        'user:view',
+        'group:view'
+    ],
+    [MANAGER_ROLE]: [
+        'user:view',
+        'group:create',
+        'group:update',
+        'group:view',
+    ],
+}
+
+module.exports = permissions;
---
+++ src/utility/userRoles.js
@@ -0,0 +1,16 @@
+const VIEWER_ROLE = 'viewer';
+const ADMIN_ROLE = 'admin';
+const MANGER_ROLE = 'manager';
+
+const USER_ROLES = {
+    VIEWER_ROLE,
+    ADMIN_ROLE,
+    MANGER_ROLE
+};
+
+module.exports = {
+    USER_ROLES,
+    VIEWER_ROLE,
+    ADMIN_ROLE,
+    MANGER_ROLE
+};
